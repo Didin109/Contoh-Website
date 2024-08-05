15edea7e8e32fdb4558ed32ce7c03cfb9d2370be
Didin109 <babepintar123@gmail.com>
2024-08-05 17:45:07 +0900

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+function orderNow() {
+    alert('Order Now button clicked!');
+    // You can replace the alert with the logic to handle the order, e.g., redirect to a WhatsApp link or an order form.
+}
